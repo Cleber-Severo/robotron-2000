dde3b97ef96ef2700cf3b939f1d47f8f5e6e2e18
Cléber Severo <clebersevero1998@gmail.com>
2022-11-19 09:46:57 -0300

    função simplificando codigo e corrigindo bug de não aparecer input foguete

--- main.js
+++ main.js
@@ -2,17 +2,22 @@ const braco = document.querySelector("#braco")
 const soma = document.querySelector("#somar")
 const subtracao = document.querySelector("#subtrair")
 
+const controle = document.querySelectorAll(".controle-ajuste")
 
-soma.addEventListener('click', () => {
+console.log(controle);
 
-    braco.value = parseInt(braco.value)+ 1 
+function alteraValor (operacao) {
 
-})
+    if (operacao === 'subtrair') {
+            braco.value = parseInt(braco.value) - 1
+    } else {
+            braco.value = parseInt(braco.value)+ 1 
+    }
 
-subtracao.addEventListener('click', () => {
+}
 
-    braco.value = parseInt(braco.value) - 1
+soma.addEventListener('click', () => { alteraValor("somar") })
 
-})
+subtracao.addEventListener('click', () => { alteraValor("subtrair") })
 
 
